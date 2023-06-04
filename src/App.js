2377dfcd3e1881a0dc12b9bc6a1d4fb3354e9ee1
Ruqayya Tocao <dcruqayya@gmail.com>
2023-06-03 20:55:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Join Hybrid Support here!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/hybridsupport.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
